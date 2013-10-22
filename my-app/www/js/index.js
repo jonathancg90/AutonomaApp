dd5851cb8c14fd4a57f362181f3846e0cece28a9
Jonathancg <Jonathancg90@gmail.com>
2013-10-22 04:35:31 -0500

    Progress

--- my-app/www/js/index.js
+++ my-app/www/js/index.js
@@ -1,38 +1,16 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
+
     onDeviceReady: function() {
+        //Eventos ya una vez cargada la aplicacion
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
@@ -45,5 +23,67 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        app.appEvent();
+        app.showAlert('Mensaje de prueba','Mensaje informativo');
+
+    },
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+//    initialize: function() {
+//        var self = this;
+//        this.store = new MemoryStore(function() {
+//            self.showAlert('Store Initialized', 'Info');
+//        });
+//        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+//    },
+
+
+    appEvent: function(){
+         var domain = 'http://redau.herokuapp.com/';
+
+        //Elementos disparadoresd e evento
+        var login = $('#logIn');
+
+        function evtLogin(){
+            debugger;
+            var data = {
+                'username':$('#username').val(),
+                'password':$('#password').val()
+            };
+            var url =  domain +'login-ajax';
+            $.ajax({
+                dataType: "json",
+                type: "POST",
+                data: data,
+                url: url,
+                success: function(data){
+                    if(data.status==false){
+                        $('.message').append(data.message);
+                    }
+                    else{
+                        alert('Usuario logeado')
+//                        $('.message').append(data.message);
+//                        $('#token').val(data.token);
+//                        $('.Information').show();
+//                        evtProfile();
+//                        evtSaberCursoDelDia();
+//                        evtCriterios();
+                    }
+                }
+            });
+
+            //Eventos
+            login.live('click',function(e){
+                debugger;
+                e.preventDefault();
+                evtLogin();
+            });
+        }
+
     }
 };
