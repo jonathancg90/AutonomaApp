2d4c7454a5307278adf8d28b2e0dde7138ba88dd
Jonathancg <Jonathancg90@gmail.com>
2013-10-22 12:53:34 -0500

    Add localstorage

---
+++ my-app/www/js/login.js
@@ -0,0 +1,38 @@
+$(window).load(function() {
+    $(document).bind('deviceready', function () {
+        var domain = 'http://redau.herokuapp.com/';
+
+        var login = $('#logIn');
+
+        login.on('click',function(e){
+            e.preventDefault();
+            evtLogin();
+        });
+
+        function evtLogin(){
+            var data = {
+                'username':$('#username').val(),
+                'password':$('#password').val()
+            };
+            var url =  domain +'login-ajax';
+            $.ajax({
+                dataType: "json",
+                type: "POST",
+                data: data,
+                url: url,
+                success: function(data){
+                    if(data.status==false){
+                        app.showAlert('Usuario no registrado','Login');
+                    }
+                    else{
+                        localStorage.myname = data.token;
+//                        $('#token').val(data.token);
+                        window.location.replace("inicio.html");
+                    }
+                }
+            });
+
+        }
+
+    });
+});
